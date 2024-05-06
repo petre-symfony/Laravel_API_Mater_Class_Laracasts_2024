516b917e4386cab1cc3a7fffb748e0ac3300f1e8
petrero <petrero@gmail.com>
2024-05-06 18:49:09 +0300

    5.2 So, with that done, we need to go to our AuthController. Now, our AuthController we created before we started designing our API. So, I want to move the AuthController into the API folder because the authentication for our API would be different from our normal web application. So, inside of here then, we need to make a few changes. The namespace is now API. We need to use app HTTP controllers and then controller. And we also need to update that inside of our route file. So, let's go to api.php and we just need to add the API to the namespace, and that should fix that. So, now for our login method, we want to accept the login user request. And the first thing we need to do is validate that data. So, we will use request validated, and we want to validate all of the request data. And if we have valid data, then we want to attempt to authenticate the user. So, we will call the attempt method, and then we will pass in only the email and the password. Now, if this fails, we need to tell the client that authentication failed. So, we have our traits for our API responses, but currently we only have successful responses. Let's have an error response. Let's copy the success and change the name to error. And really, except for having a default value for status code, I think that that is going to be just fine. We still have the message, the status, and everything is good there. So, when the authentication fails, then we will call our error, and we'll just say invalid credentials. And then for the HTTP status code, we'll say 401 for unauthorized. But if authentication is successful, then we need to fetch the user from the database. So, let's call first where the email is the same as the email provided in the request, and then we will return an okay response.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
+use App\Http\Controllers\Api\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
