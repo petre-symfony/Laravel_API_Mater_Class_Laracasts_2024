7a82ba3ef12b9e30fe50cba7745446e71c4f3988
petrero <petrero@gmail.com>
2024-05-06 11:40:25 +0300

    4.2 Now, if we look at the requests folder, it's going to be a little bit different because it created those requests directly inside of the requests folder.  And instead I would want to do the same thing for the requests folder. I would want to do the same thing for the requests that we did with the controllers so that we would have requests and then API and then V1. And then we would have our requests.  That way we just keep things structured the same and we aren't mixing and matching our request objects. We do need to change our namespace here. So this is API V1. We need to make the same change inside of update ticket, but then we also need to make that change inside of our controller. So let's go back to the tickets controller and let's change this use statement and that should get that fixed up.  So what we can do inside of the index method, we will simply return. We will use our ticket model. And for right now, we'll just return all of our tickets. We won't worry about storing or anything else like that, at least for right now. And that should get us going.

--- app/Http/Controllers/Api/v1/TicketController.php
+++ app/Http/Controllers/Api/v1/TicketController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreTicketRequest;
-use App\Http\Requests\UpdateTicketRequest;
+use App\Http\Requests\Api\v1\StoreTicketRequest;
+use App\Http\Requests\Api\v1\UpdateTicketRequest;
 use App\Models\Ticket;
 
 class TicketController extends Controller {
@@ -12,7 +12,7 @@ class TicketController extends Controller {
 	 * Display a listing of the resource.
 	 */
 	public function index() {
-		//
+		return Ticket::all();
 	}
 
 	/**
