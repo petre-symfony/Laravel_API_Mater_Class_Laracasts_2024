6c1d19fb745695ee05b00174db6861987857dfff
petrero <petrero@gmail.com>
2024-05-06 09:02:39 +0300

    2.1 Working with Postman  I am a woodworker, and one thing I learned very early on is that the right tool makes all the difference in the world. And if you don't have that tool, yes, you can get around that, but your results aren't going to be as good, and it's definitely going to take you a lot longer to achieve whatever it is that you were trying to achieve.  And the same goes for writing applications, especially web APIs, because we need a tool that we can use to make HTTP requests. And we have one. It's called the browser. In fact, in the previous lesson, we made a GET request for our login route, and we can see the response in the browser, but we don't need a GET request here. We need a POST request.  And if we change this to POST, yes, we can write a form to submit that, or we can write some JavaScript, but that's going to take extra time. And really what we need is a tool that we can use to not just make HTTP requests, but also inspect the responses. We need what's called an HTTP debugger.  And there are many available, but one that I typically use is called Postman. Now, they do have some pricing tiers, but you can use Postman for free. And it is one of those applications where they want you to sign in, and when you do sign in, you get a lot of extra features than if you don't sign in. But you can use it however you want. Just download and install it. If you want to sign in, great. It gives you the ability to create a workspace.  So if you are working on multiple APIs, you can create workspaces for those individual projects. I'm going to create a workspace, I'll call it Tickets Please, which, by the way, this is for creating and managing support tickets. I don't think I mentioned that in the previous lesson, but that's the general idea. And once you have a workspace, just click on the plus sign up at the top, and this is going to create a new request.  So let's make a request for our login route. That was API and login. And we will see the result in the response, except, no, we won't, because I just changed that to post, didn't I? Yes, I did.  But that's one of the beautiful things about something like Postman, is that we can easily change the type of request. We now have a post request, we can send that, and we can see that we are getting the appropriate response.  Now, of course, in order to log in, we need to supply some credentials. And we don't have anything as far as authentication is concerned yet, but we can go ahead and we can set this request up so that whenever we do have authentication, well, this will work.  So let's make a request. We'll use Artisan to make a request, and we'll call this API Login Request - php artisan make:request ApiLoginRequest.  And as far as a login request is concerned, we will essentially just have two pieces of information. We'll have the email address and the password. So let's add some validation rules, because we need to be sure we have those pieces of information.  Let's change the authorize to true, because if it's false, then the user is not going to be able to log in, because they will not be authorized to do that. So we will set that to true. And then inside of rules, we will have the email field, which is going to be required. Ideally, we would have some other validation here, and password is going to be required as well.

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::post('/', function () {
     return view('welcome');
 });
