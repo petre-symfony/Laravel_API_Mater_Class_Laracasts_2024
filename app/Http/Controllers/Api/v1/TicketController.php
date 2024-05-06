3621e7c41674b9efde06d85f9df1956d1b242e70
petrero <petrero@gmail.com>
2024-05-06 16:54:54 +0300

    4.4 So instead of using resource, we can use api resource.  And this is going to set up the routes for only the things that we need. So this is going to give us the ability for tickets, for getting the ID, and for storing tickets. But it's not going to set up routes for the unnecessary things for an API, which would be for showing forms.  So this create method inside of our tickets controller, we can just get rid of because we don't need it. The same is true for the edit method. So we can get rid of that, and we'll leave everything else there.

--- app/Http/Controllers/Api/v1/TicketController.php
+++ app/Http/Controllers/Api/v1/TicketController.php
@@ -16,13 +16,6 @@ class TicketController extends Controller {
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 */
-	public function create() {
-		//
-	}
-
-	/**
 	 * Store a newly created resource in storage.
 	 */
 	public function store(StoreTicketRequest $request) {
@@ -33,13 +26,6 @@ class TicketController extends Controller {
 	 * Display the specified resource.
 	 */
 	public function show(Ticket $ticket) {
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 */
-	public function edit(Ticket $ticket) {
 		//
 	}
 
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
+use App\Http\Controllers\Api\v1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('tickets', \App\Http\Controllers\Api\v1\TicketController::class);
+Route::apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
