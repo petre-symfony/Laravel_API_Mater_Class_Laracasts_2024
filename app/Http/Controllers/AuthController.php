2f40d01d59593e721aaaf2e7d1a10fc84d846bc7
petrero <petrero@gmail.com>
2024-05-06 09:38:55 +0300

    2.2 And then we can use this request inside of our auth controller. For our login method, we will type hint for the API Login Request.  And instead of outputting hello login, let's do this. We will output the email address that was supplied. So we will get the email from the request, and we should see that.  So let's go to Postman. Now we need to be able to send this information with the request, and we can do so by going to this Body tab here. And we want to set this to form data, and then we will set the keys and values. So our first key is going to be email. The value is going to be whatever email address that we want to use. Let's use jeremy at mcpeak.com. And then for the password, let's have password 1234.  And whenever we submit this, we should see the same structure, except that the message is now the address that we included.  And of course, whatever we pass as the email address is going to be represented in the message that we get back from the server.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller {
 	use ApiResponses;
 
-	public function login() {
-		return $this->ok('Hello login');
+	public function login(ApiLoginRequest $request) {
+		return $this->ok($request->get('email'));
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
