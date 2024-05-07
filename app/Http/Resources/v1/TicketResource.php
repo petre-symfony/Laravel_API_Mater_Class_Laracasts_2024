ec7368e8094787be12d87227f803d52452d3d5d0
petrero <petrero@gmail.com>
2024-05-07 12:00:30 +0300

    7.5 I don't want to do that though. I want to adhere to the JSON API specification. So we're going to leave that commented out just so that it's in the code, but I want our wrapper to be data. And then we just need to start organizing our data in this particular format.  So there's this type property which is required. There's this ID that is required as well. And so we can start there. So all we need to do inside of toArray is return an array of our structure. And we'll start with the type. So that was ticket in our case. Then we would have the ID, which would be, of course, the ticket ID.  And once again, if we just made a request now, we see that our data changes. Now we only see the information that is returned from our resource.  So this is how we can start to omit things if we need to omit them from the response. And later on, we'll look at how we can conditionally include or omit things. But this is it.  I mean, the concept is very simple. We simply define the things that we want inside of our JSON payload, and that's going to be it.

--- app/Http/Resources/v1/TicketResource.php
+++ app/Http/Resources/v1/TicketResource.php
@@ -6,13 +6,16 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TicketResource extends JsonResource {
-	public static $wrap = 'ticket';
+	//public static $wrap = 'ticket';
 	/**
 	 * Transform the resource into an array.
 	 *
 	 * @return array<string, mixed>
 	 */
 	public function toArray(Request $request): array {
-		return parent::toArray($request);
+		return [
+			'type' => 'ticket',
+			'id' => $this->id
+		];
 	}
 }
