3820e25d95d8d07ae19c2086951919587ee95a01
petrero <petrero@gmail.com>
2024-05-09 11:08:40 +0300

    10.8 We could do something similar to the status for the created at.  So we could say that the client could provide a single date or they could provide a comma-separated list of dates for a range. So we could do something like this to where we will explode, based upon a comma, the value. And if we have more than one date, then we know that we have a range that we need to filter. So we will return this builder where between, and we want the created at field between the provided dates. However, if we just have one date, then we just need to return this builder where date, and then we specify created at is the provided date, and that should work there. And since we have that, we can go ahead and we can also implement the updated at. I'm putting these in alphabetical order just because that's just me. So we have our updated at. We need to change the field here. So that is updated at, and that is updated at, but everything else should be fine.  So let's try the title filter. We should get just the completed and canceled. So the title there, the status, that looks like that that works. Title status. Yeah, we are good there. Okay, so let's look at the dates. They probably have the same date since these were all generated on the same date. That's a little difficult. So let's change this to id. So, okay, we have, that was created on the second. I tell you what, let's just do this. Let's get rid of all of the other filters and let's just filter the created at, and we want the date of 2024, February 1st, and we should have one or two there, and it looks like we do. So that works fine.  Let's do a range of 2024-02-03, 2024-02-05. That should give us one or two, but we see that we have a created at date of the third, of the fourth, and great. That worked fine as well.  So we have our basic filters all set up and ready to go. We are able to filter all of the attributes on our ticket. We are also able to include the author information, and we are able to do all of that from our controller with one line of code.  So now the question, at least to me, becomes how do we filter the tickets based upon the user information?  So let's say, for example, we wanted the tickets that have a status of completed, but we only want those completed tickets that were submitted by the user with an ID of five.  Well, we could add more filters here based upon the user information, but we also need to think about this, because if we have the ID of the user, we really shouldn't be going through the tickets. We should be going through the users.  So if we wanted the tickets by the user with an ID of five, we should start there.  And yes, we can include the tickets, but I think it would be great if we could also have relationships or have just tickets. I think tickets is going to be fine.  - localhost:8000/users/5/tickets  That would give us the tickets that were submitted by that user, and then we could filter it from there. And we will look at how to do that in the next episode.

--- app/Http/Filters/v1/TicketFilter.php
+++ app/Http/Filters/v1/TicketFilter.php
@@ -3,6 +3,16 @@
 namespace App\Http\Filters\v1;
 
 class TicketFilter extends QueryFilter {
+	public function createdAt($value) {
+		$dates = explode(',', $value);
+
+		if (count($dates) > 1) {
+			return $this->builder->whereBetween('created_at', $dates);
+		}
+
+		return $this->builder->whereDate('created_at', $value);
+	}
+
 	public function include($value) {
 		return $this->builder->with($value);
 	}
@@ -15,5 +25,15 @@ class TicketFilter extends QueryFilter {
 		$likeStr = str_replace('*', '%', $value);
 
 		return $this->builder->where('title', 'like', $likeStr);
+	}
+
+	public function updatedAt($value) {
+		$dates = explode(',', $value);
+
+		if (count($dates) > 1) {
+			return $this->builder->whereBetween('updated_at', $dates);
+		}
+
+		return $this->builder->whereDate('updated_at', $value);
 	}
 }
