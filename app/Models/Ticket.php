b498f51b922e9680f83513705a5d1553b4eb1199
petrero <petrero@gmail.com>
2024-05-09 10:16:21 +0300

    10.5 Of course, however, we didn't say user, did we?  We said include author, and that's going to make us make a few changes. But really, all we would need to do is change user to author here. We would specify the user ID as the foreign key. We do need to go to our ticket resource because we also said to load the user when it's loaded. So here we will say author.  So with that done, let's try that now. We should see the same results. We have includes. Okay, so that's great.

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -11,8 +11,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Ticket extends Model {
 	use HasFactory;
 
-	public function user(): BelongsTo {
-		return $this->belongsTo(User::class);
+	public function author(): BelongsTo {
+		return $this->belongsTo(User::class, 'user_id');
 	}
 
 	public function scopeFilter(Builder $builder, QueryFilter $filters) {
