db9a411806c076f9fb0400d44095867f61e53708
petrero <petrero@gmail.com>
2024-05-09 10:01:13 +0300

    10.2 But for right now, let's just make this work. So this means that we need this builder, but we could get this builder from here, this locally scoped filter method. So let's go to our ticket model and let's add that method. So we'll have public function scope filter. And we will get the query builder as the first argument here. And then from here, we'll get the ticket filter. And then we can say that our filters is going to have an apply method. And that's how we will supply the builder to our ticket filter.

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Http\Filters\v1\TicketFilter;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,5 +13,9 @@ class Ticket extends Model {
 
 	public function user(): BelongsTo {
 		return $this->belongsTo(User::class);
+	}
+
+	public function scopeFilter(Builder $builder, TicketFilter $filters) {
+		return $filters->apply($builder);
 	}
 }
