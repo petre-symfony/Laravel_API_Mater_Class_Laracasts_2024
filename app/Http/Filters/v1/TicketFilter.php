9b4ac403dba217c86a4d49cf4b29c0597ab3423c
petrero <petrero@gmail.com>
2024-05-09 10:12:42 +0300

    10.4 Now let's include, because we want to have that functionality as well, the user.  Now, of course, we don't have that yet, but we could go back to our ticket filter and we could simply add that include method to where then we would simply return the builder with, and then that provided value.  So now we should get not only the completed tickets, but we should get the user information there, and sure enough, we do.  Let's make sure I'm getting the status as C. Yes, that's working just fine.

--- app/Http/Filters/v1/TicketFilter.php
+++ app/Http/Filters/v1/TicketFilter.php
@@ -3,6 +3,10 @@
 namespace App\Http\Filters\v1;
 
 class TicketFilter extends QueryFilter {
+	public function include($value) {
+		return $this->builder->with($value);
+	}
+
 	public function status($value) {
 		return $this->builder->where('status', $value);
 	}
