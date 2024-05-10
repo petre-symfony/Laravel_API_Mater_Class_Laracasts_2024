8688236e2cad773b7f0ba9681cce1e21ee292bbb
petrero <petrero@gmail.com>
2024-05-10 10:30:57 +0300

    13.3 So we need to add a body.  This is going to be a JSON payload, so let's start there to where we will have our data, which will then have our attributes, which will then have title. And let's call this first ticket, because really this is the first ticket that we are creating. And if we send this, of course, we will see that the title is now validated. Everything is fine there.  Now we just have the description status and the author ID. So let's add the description, which can be something descriptive for the first ticket. This is the first ticket we created. And let's go ahead and add the status. But let's add an invalid status like T, because, well, why not? And if we submit this, we will then see, well, we're seeing what I did not expect. Validate string, in does not exist.  Did I type something wrong? Let's look at our rules, and yes, that needs to be a pipe, not a comma. So there we go.  Let's resend this, and we can see that the selected data.attributes.status is invalid. Yeah, that's okay, because it is invalid.

--- app/Http/Requests/Api/v1/StoreTicketRequest.php
+++ app/Http/Requests/Api/v1/StoreTicketRequest.php
@@ -22,7 +22,7 @@ class StoreTicketRequest extends FormRequest {
 		return [
 			'data.attributes.title' => 'required|string',
 			'data.attributes.description' => 'required|string',
-			'data.attributes.status' => 'required|string,in:A,C,H,X',
+			'data.attributes.status' => 'required|string|in:A,C,H,X',
 			'data.relationships.author.data.id' => 'required|integer'
 		];
 	}
