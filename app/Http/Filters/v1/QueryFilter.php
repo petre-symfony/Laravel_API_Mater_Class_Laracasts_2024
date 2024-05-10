3ed8ad338212ba83f09138e559bba75a95f2547a
petrero <petrero@gmail.com>
2024-05-10 09:08:21 +0300

    12.2 And now let's talk about our sort. So our sort, of course, is going to be provided via the query string.  We could have a sort parameter. And in fact, we could do something like we did with the filter. So that we could have sort and then we could specify the column as a key, like for the title. And then we could have the value as being ascending or descending. And that would work.  But, you know, then if we wanted to sort by multiple columns, then we would have to have sort and then like status. And then it's just, it gets very verbose very quickly.  And I'm not sure I want to do that for something as simple as sorting. Because with sorting, we have two values, ascending, descending, that's it. So maybe we should do something like this, to where we would have just a single sort query string parameter. And then we could have just a comma separated value, so that we could have title. And if we wanted to also then filter or sort by status, I'm used to saying filter. If we wanted to sort by title and then status, then we can do that just separate them with a comma. And then that would work. And the default would be ascending sort. And then we could specify a descending sort by putting a minus sign before the column name.  And I think that that's fine. It's nice, simple, clean. Yeah, let's do it.  So let's do this inside of our query filter. Because we have all of the plumbing set up already to parse out stuff in the query string. So from a functionality standpoint, it just kind of makes sense to do this here. I'm going to reorder this so that we are in alphabetical order, because I'm that way. nd then we will essentially do what we did like for the filter.  So that we will have a protected function, we'll call it sort, to where we will get the parameter value. And since this can be a comma-separated list of values, let's first of all convert that into an array. So we'll have a variable called sort attributes to where we will explode on a comma for the value. And then we could iterate over each one of these attributes. And we'll just call that as a sort attribute. And here we need to determine the direction of our sort. And we'll say that the default is going to be an ascending sort. So to check to see if we need a descending sort, we can check the first character in the sort attribute. And if it is a minus sign, then we know that this is a descending sort. So we will need to change the direction to descending. But then we can also go ahead and we can normalize our sort attribute here. Because this is going to have a minus sign in front of it, we can take that out by getting the substring of our sort attribute, starting with the position of one. So that will give us just the column name that we need there. And since we have the direction, all we would need to do is use the builder, order by the sort attribute, and then the direction. And that should work.  So let's go back to Postman and let's sort by title. And we should start with the As we do. So it looks like that that's going to work fine. If we say to sort in descending order for our title, yes, that works just fine as well.  Let's change that so that we now sort by the status. We should start with the status of A for the active statuses. But then let's say that, okay, we want to then sort by title descending. In which case, yep, so we are sorting by the status and then title in descending order. So yeah, that's looking great.

--- app/Http/Filters/v1/QueryFilter.php
+++ app/Http/Filters/v1/QueryFilter.php
@@ -13,16 +13,6 @@ abstract class QueryFilter {
 		$this->request = $request;
 	}
 
-	protected function filter($arr) {
-		foreach($arr as $key => $value) {
-			if (method_exists($this, $key)) {
-				$this->$key($value);
-			}
-		}
-
-		return $this->builder;
-	}
-
 	public function apply(Builder $builder) {
 		$this->builder = $builder;
 
@@ -33,5 +23,30 @@ abstract class QueryFilter {
 		}
 
 		return $builder;
+	}
+
+	protected function filter($arr) {
+		foreach($arr as $key => $value) {
+			if (method_exists($this, $key)) {
+				$this->$key($value);
+			}
+		}
+
+		return $this->builder;
+	}
+
+	public function sort($value) {
+		$sortAttributes = explode(',', $value);
+
+		foreach ($sortAttributes as $sortAttribute) {
+			$direction = 'asc';
+
+			if (strpos($sortAttribute, '-') === 0){
+				$direction = 'desc';
+				$sortAttribute = substr($sortAttribute, 1);
+			}
+
+			$this->builder->orderBy($sortAttribute, $direction);
+		}
 	}
 }
