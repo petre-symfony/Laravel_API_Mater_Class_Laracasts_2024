a9f0bd5fd98efdd067c716343df92a85cc5f688c
petrero <petrero@gmail.com>
2024-05-10 12:13:02 +0300

    14.3 So let's do this. Let's go to the show method, not the store method, and let's wrap all of this within a try and catch. In fact, let's just copy what we have for our delete method and paste that in so that I don't have to type all of that, and we might run into the same thing. Yeah. So let's do this. For our show method, we will get the ticket ID, and we will try to get the ticket with that ID, and we'll just do that right here. We don't need to change any other part of the code. We'll just do find or fail this time, passing in the ticket ID, and everything else should work fine.  So if we send that request once again, we see that the ticket cannot. I just said this needs to be at least. There we go. Ticket cannot be found. Okay. So let's. All right. Okay. So that's working. Okay.

--- app/Http/Controllers/Api/v1/TicketController.php
+++ app/Http/Controllers/Api/v1/TicketController.php
@@ -44,12 +44,18 @@ class TicketController extends ApiController {
 	/**
 	 * Display the specified resource.
 	 */
-	public function show(Ticket $ticket) {
-		if ($this->include('author')) {
-			return new TicketResource($ticket->load('user'));
-		}
+	public function show($ticket_id) {
+		try {
+			$ticket = Ticket::findOrFail($ticket_id);
+			
+			if ($this->include('author')) {
+				return new TicketResource($ticket->load('user'));
+			}
 
-		return new TicketResource($ticket);
+			return new TicketResource($ticket);
+		} catch (ModelNotFoundException $exception) {
+			return  $this->error('Ticket Not Found', 404);
+		}
 	}
 
 	/**
