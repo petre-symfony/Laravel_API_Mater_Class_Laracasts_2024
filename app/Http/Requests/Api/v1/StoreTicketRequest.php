02e35bc8c8e40f149094d42942873f7822b840cf
petrero <petrero@gmail.com>
2024-05-18 20:41:59 +0300

    22.1 The Principle of Least Privilege  To give you an idea of how I create these videos, I typically batch record during the week and then I batch edit on the weekend, primarily because I want to break between the two. By the time I'm done recording an episode, the last thing I want to do is hear myself talk about what I just recorded. It's just, I'm tired of hearing my voice.  That gives me an opportunity to go back over the things that was covered, the code that I wrote or presented, and just to see how stupid I am sometimes, because we all do it. We all write code and we all return to that code at some other point and we think, what in the world were we thinking? And yeah, permissions are one of those things.  Let's look at the store user request, because this set off red flags, and no, that wasn't the right one. The store ticket request. The set off red flags, I knew this was not really the best approach, but I wanted to get it done so that we can move on. How many times do we do that and then we regret it? So, okay, as a refresher, this is a request that's being used by the ticket controller and the author ticket controller. And so we needed to get the appropriate author ID attribute so that we could assign the correct rules for that.  And in this particular case, the default rule was for like a manager or an admin so that they could create a ticket for any user. But then we checked to see, well, if the user can only create their own tickets, well, then we need a more restrictive rule. And it works, but it forced us, or forced me rather, to add this comment to the get abilities method on the abilities class. And that was to not assign an asterisk, because if a user has an asterisk as an ability, this if statement is going to be true. Therefore, it's going to limit their ability to create tickets for other users.  And that's not the approach. That is the wrong thing to do. I should have designed that a little bit better. And it's really not that hard. All we need to do is follow the concept of least privilege or the principle of least privilege. If you're not familiar with this, it's not a programming thing. It's just a security thing. The idea is that regardless of what the user is, you assign the most restrictive permissions for that user. And then if they need more authority, you grant them that authority.  So if we were going to follow the principle of least privilege, we would do two things here. The first would be that for every user, it didn't matter if they were a typical user or a manager or an admin, they would be assigned the create own ticket, update on ticket and delete on ticket abilities, because those are the least privilege. But then if the user was a manager, we would also assign these other abilities. And if it was an admin, it wouldn't matter. We'd give them the asterisk so that they could do all of the things. And we don't necessarily need to do that here.  But we do, however, need to apply the principle of least privilege in our rules. So for the sake of creating a new ticket, we need to set as the default validation rules the most restrictive rules. That means that by default, the author ID attribute needs to match the signed in user's ID, because that is the least privilege for any user. But then if we needed to grant more authority, like for example, for a manager, if they can create a ticket, then we modify the rule so that they can create that ticket. So this means for all users that can only create a ticket for themselves unless if they have the authority to create tickets for anyone. So that would be a manager, or if an admin had an asterisk, then this would grant them the ability to create a ticket for any user.

--- app/Http/Requests/Api/v1/StoreTicketRequest.php
+++ app/Http/Requests/Api/v1/StoreTicketRequest.php
@@ -20,18 +20,19 @@ class StoreTicketRequest extends BaseTicketRequest {
 	 */
 	public function rules(): array {
 		$authorizeIdAttr = $this->routeIs('tickets.store') ? 'data.relationships.author.data.id' : 'author';
+		$user = $this->user();
 
 		$rules = [
 			'data.attributes.title' => 'required|string',
 			'data.attributes.description' => 'required|string',
 			'data.attributes.status' => 'required|string|in:A,C,H,X',
-			$authorizeIdAttr => 'required|integer|exists:users,id'
+			$authorizeIdAttr => 'required|integer|exists:users,id|size:' . $user->id
 		];
 
 		$user = $this->user();
 
-		if ($user->tokenCan(Abilities::CreateOwnTicket)) {
-			$rules[$authorizeIdAttr] .= '|size:' . $user->id;
+		if ($user->tokenCan(Abilities::CreateTicket)) {
+			$rules[$authorizeIdAttr] = 'required|integer|exists:users,id';
 		}
 
 		return $rules;
