c91ddb9d3fe4e20f6b7183385b276920400254fe
petrero <petrero@gmail.com>
2024-05-19 11:51:27 +0300

    23.3 Let's do this, though. Outside of our handler, I don't want to provide the fully qualified class name. So let's start by writing a class name variable, where we will call getClass an exception. Then we will get the index of the last set of double backslashes. Then for the type, we can call substring, pass in the class name, and we want to start at the index plus one.  So now that should give us just NotFoundHttpException. For our validation exception, we should get just ValidationException. So now for these other kinds of exceptions, I want to provide some custom rendering.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,9 +23,13 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
     	$exceptions->render(function (Throwable $e, Request $request) {
+				$classname = basename(get_class($e));
+				
+				$index = strrpos($classname, '\\');
+
 				return response()->json([
 					'error' => [
-						'type' => basename(get_class($e)),
+						'type' => substr($classname, $index + 1),
 						'status' => 0,
 						'message' => $e->getMessage(),
 						'source' => 'Line! ' . $e->getLine() . ': ' . $e->getFile()
